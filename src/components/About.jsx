b5d1a6f355e478ff19773dc6a055de922d4fae9a
CHINTHAKAYALA BALAJI <chinthakayalabalaji1322@gmail.com>
2026-07-03 23:51:51 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,16 @@ export default function About() {
       <h2 className="section-title">About Me</h2>
       <p className="section-subtitle">Who I am and what I love working on</p>
       <p className="about-text">
-        I'm a passionate Full Stack Developer currently pursuing my B.Tech in Computer
-        Science and Engineering at Sri Venkateswara College of Engineering (CGPA: 8.1).
-        I specialize in the MERN stack, backend architecture, RESTful APIs, and scalable
+        I'm a passionate Java Developer Graduated in(B.Tech) 
+        Computer Science and Engineering at Sri Venkateswara College of Engineering (72.1%).
+        I specialize in the Java Fullstack, backend architecture, RESTful APIs, and scalable
         database design.
         <br />
         <br />
-        I enjoy turning ideas into real products — from building authentication systems
-        and admin panels to creating smooth user experiences. Outside of development,
-        I continuously challenge myself with Data Structures & Algorithms, having solved
-        over 500 problems across coding platforms.
+        Aspiring Software Development Engineer with strong foundations in Java, Spring Boot, SQL, and web technologies.
+        Passionate about building scalable applications, developing REST APIs, and solving engineering problems through
+        software solutions.
+
       </p>
     </section>
   );
